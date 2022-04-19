37a2476748b5a295b47a2b5279ab0c56f13765f7
Vitaliy Viktorovich <po4tavela@gmail.com>
2022-04-19 14:33:18 +0300

    prepare to rewrite network auth logic

--- GB_1640_Shop/App/Entity/User.swift
+++ GB_1640_Shop/App/Entity/User.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct User: Codable {
-    let id: Int
-    let login: String
-    let password: String
-    let name: String
-    let lastname: String
-    let biography: String
-    let creditcard: String
-    let email: String
-    let gender: String
+    let id: Int?
+    let login: String?
+    let password: String?
+    let name: String?
+    let lastname: String?
+    let biography: String?
+    let creditcard: String?
+    let email: String?
+    let gender: String?
 
     enum CodingKeys: String, CodingKey {
         case id = "id_user"
@@ -26,5 +26,19 @@ struct User: Codable {
         case lastname = "user_lastname"
         case biography = "user_biography"
         case creditcard, email, gender
+    }
+}
+
+public struct DemoData {
+   static func returnUser() -> User {
+        return User(id: 123,
+                    login: "Somebody",
+                    password: "Password",
+                    name: "Name",
+                    lastname: "Lastname",
+                    biography: "Bio bio bio bio bio bio bio bio bio bio bio bio",
+                    creditcard: "1234-1234-1234",
+                    email: "some@some.com",
+                    gender: "X")
     }
 }
